913538179db70b322d3860f0ce8b2f25a32e434d
Lisina <lisina.pro@gmail.com>
2026-04-01 22:04:14 +0300

    The second task is fully completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,85 @@
-function getArrayParams(...arr) {
+"use strict";
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+		if(arr[i] > max) {
+			max = arr[i];
+		}
+		if(arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	const avg = parseFloat((sum / arr.length).toFixed(2));
 }
 
 function summElementsWorker(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+		if(arr[i] > max) {
+			max = arr[i];
+		}
+		if(arr[i] < min) {
+			min = arr[i];
+			return;
+			min;
+			max;
+			sum;
+		}
+	}
 
+	function summElementsWorker(...arr) {
+		let sum = 0;
+		for(let num of arr) {
+			sum += num;
+		}
+		return sum;
+	}
+
+	function differenceMaxMinWorker(...arr) {
+		const max = Math.max(...arr);
+		const min = Math.min(...arr);
+		return max - min;
+	}
+
+	function differenceEvenOddWorker(...arr) {
+		let sumEvenElement = 0;
+		let sumOddElement = 0;
+		for(let num of arr) {
+			if(num % 2 === 0) {
+				sumEvenElement += num;
+			} else {
+				sumOddElement += num;
+			}
+		}
+		return sumEvenElement - sumOddElement;
+	}
+
+	function averageEvenElementsWorker(...arr) {
+		let sumEvenElement = 0;
+		let countEvenElement = 0;
+		for(let num of arr) {
+			if(num % 2 === 0) {
+				sumEvenElement += num;
+				countEvenElement++;
+			}
+		}
+		if(countEvenElement === 0) return 0;
+		return parseFloat((sumEvenElement / countEvenElement).toFixed(2));
+	}
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	const currentResult = func(...arr);
+	if(currentResult > maxWorkerResult) {
+		maxWorkerResult = currentResult;
+	}
 }
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+return maxWorkerResult;
