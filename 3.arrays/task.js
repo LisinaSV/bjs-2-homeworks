d0b873883bf06a0e0a518dff84ac274fbd5acf04
Lisina <lisina.pro@gmail.com>
2026-04-12 22:28:03 +0300

    Solutions for 3 tasks have been added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+	if(arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((element, index) => {
+		return element === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if(!users || users.length === 0) {
+		return 0;
+	}
+	const filteredUsers = users.filter(user => user.gender === gender);
+	if(filteredUsers.length === 0) {
+		return 0;
+	}
+	const sumAge = filteredUsers.reduce((sum, user) => {
+		return sum + user.age;
+	}, 0);
+	return parseFloat((sumAge / filteredUsers.length).toFixed(1));
 }
